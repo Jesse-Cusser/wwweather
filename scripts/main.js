47cafaf3b2be77ec0ca3f21011fde6f28f719eb9
Maverink <juliomaurocarvalho@hotmail.com>
2017-11-02 16:25:43 +0000

    create css and switch temp feature

--- scripts/main.js
+++ scripts/main.js
@@ -9,29 +9,46 @@ $(document).ready(function(){
 			var lat  = position.coords.latitude;
 			var long = position.coords.longitude;
 
-			var url = "http://api.openweathermap.org/data/2.5/weather?&id=2172797";
-			var apiKey = "APPID=90c2aa55436224d6c901df2dfcaf23ed"
-			var api = url + "&" + apiKey + "&lat=" + lat + "&lon=" + long;
- 
+			var weatherUrl = "http://api.openweathermap.org/data/2.5/weather?&id=2172797";
+			var weatherApiKey = "APPID=90c2aa55436224d6c901df2dfcaf23ed"
+			var weatherApi = weatherUrl + "&" + weatherApiKey + "&lat=" + lat + "&lon=" + long;
+ 			
+ 			
+ 			
 			
 
-			$("#data").html("latitude: " + lat
-			+ "<br>longitude: " + long);
+			
 
 		
 
 
-		$.getJSON(api, function(data){
+		$.getJSON(weatherApi, function(data){
 		//JSON call for open weather api
 
-		var weatherType = data.weather[0].description;
-		var temp = data.main.temp;
-		var windSpeed = data.wind.speed;
-		var city =data.name;
-		alert(city + " " + weatherType+ " " + "temp" );
+			var weatherType = data.weather[0].description;
+			var kTemp = Math.round(data.main.temp);
+			var cTemp = Math.round(kTemp - 273.15);
+			var fTemp = Math.round(1.8 * cTemp + 32);
+			var windSpeed = data.wind.speed;
+			var city =data.name;
+			var tempSwap = false;
+			
 
-		alert(lat);
-		alert(city);
+			$("#city").html(city);
+			$("#weatherType").html(weatherType);
+			$("#temp").html(cTemp + "C*");
+			$("#windSpeed").html(windSpeed);
+			
+			$("#temp").click(function(){
+				if(tempSwap === false){
+					$("#temp").html(fTemp + "F*");
+					tempSwap=true;
+				}else{
+					$("#temp").html(cTemp + "C*");
+					tempSwap=false;
+				}
+
+			});
 
 	});
 		});
